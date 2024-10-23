6115d4d9dfbbd86ff7e65ec6a0b0b65366ea0347
khangtran2020 <kt36@njit.edu>
2024-10-22 23:52:05 -0400

    add debug mode

--- scripts/generate/generate.py
+++ scripts/generate/generate.py
@@ -24,7 +24,7 @@ def run(args, filepath: str, csvpath: str):
     # init model
     model = MODEL_DICT[args.model]
     llm = LLM(
-        model=model, dtype="float16", max_model_len=8192, gpu_memory_utilization=0.9
+        model=model, dtype="float16", max_model_len=8192, gpu_memory_utilization=0.3
     )
     tokenizer = llm.get_tokenizer()
     temperature = 0.2
